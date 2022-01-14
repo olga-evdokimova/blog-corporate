3a03d52d15ddc0e18a504739eb13cf677a433342
Olga Evdokimova <pavedom13@gmail.com>
2022-01-14 20:53:34 +0300

    правки

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,9 +1,11 @@
 //Плавное появление тукста при загрузке
 const headerTitle = document.querySelector('.header-title');
 const breadcrumbs = document.querySelector('.breadcrumbs');
+const anim = document.querySelectorAll('.anim');
+
 
 window.onload = () => {
-    headerTitle.classList.add('visible');
-    breadcrumbs.classList.add('visible');
-
+    headerTitle.classList.add('visible-text');
+    breadcrumbs.classList.add('visible-text');
+    anim.forEach((item) => item.classList.add('visible'));
 }
